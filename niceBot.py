cedec33a81d79a555c4c982d15722cde5811d589
CharlesAverill <charlesaverill20@gmail.com>
2020-11-28 23:41:11 -0600

    sort of working, neural net + grading scale are off

--- niceBot.py
+++ niceBot.py
@@ -1,4 +1,6 @@
 import discord
+import text2emotion as te
+import math
 
 
 class MyClient(discord.Client):
@@ -6,6 +8,10 @@ class MyClient(discord.Client):
     def __init__(self, **options):
         super().__init__(**options)
         self.count = 0
+        # If these words are in a message they are probably sad
+        self.keywords = ["sad", "failed", "die", "dead", "mad", "cry", "...", "worried", "scared", "frightened", "fuck",
+                         "shit", "sucks", "damn", "die", "die", "cry", "so sad", "really sad", "really worrying",
+                         "wrong", "went wrong", "really bad", "worthless", "meaningless", "depressed", "hate", "death"]
 
     async def on_ready(self):
         print('Logged on as', self.user)
@@ -14,7 +20,22 @@ class MyClient(discord.Client):
         if message.author == client.user:
             return
 
-        await message.channel.send(message.content + " " + str(self.count))
+        # Sadness = emotion_reading(0, 1.0) + message_len(0, 1.0) + (2 * log(num of sad keywords in message))
+
+        emotion_reading = te.get_emotion(message.content)["Sad"]
+
+        # theory is "longer messages are sadder usually"
+        # 2000 is discord character limit
+        message_len = len(message.content) / 2000.0
+        sad_keywords = 0
+
+        for word in self.keywords:
+            if word in message.content:
+                sad_keywords += 1
+
+        sadness_value = (emotion_reading + message_len) * (2 * math.log(sad_keywords + 1))
+
+        await message.channel.send(sadness_value)
         self.count += 1
 
 
